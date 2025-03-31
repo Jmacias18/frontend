6e3f5df7a88c11e7b2532a319476975154484e06
Jmacias18 <122376773+Jmacias18@users.noreply.github.com>
2025-03-30 19:24:25 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0', // Permite acceso desde cualquier IP
-    port: 3000, // Puedes cambiarlo si es necesario
+    port: 5200, // Puedes cambiarlo si es necesario
     allowedHosts: [
       "r0g4kww4wwk08k4k00wosw8c.31.170.165.191.sslip.io"
     ],
