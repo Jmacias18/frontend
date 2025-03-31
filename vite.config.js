30726afdbb6c6dbf7da190913edb93b99e0b3b56
Jmacias18 <122376773+Jmacias18@users.noreply.github.com>
2025-03-30 19:21:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Puedes usar '0.0.0.0' si quieres acceder desde otra red
+    port: 5200, // Cambia el puerto aquí
+  },
+});
